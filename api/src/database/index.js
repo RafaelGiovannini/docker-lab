99cf3e6bc13da9fc47fd8d70053eab79b80db538
DESKTOP-J30QPCP\rafag <rafael.marques@7comm.com.br>
2020-08-17 12:22:36 -0300

    env

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -1,33 +1,23 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-mongoose.connect('mongodb://db:27017/dockercat');
+const {
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB
+  } = process.env;
+  
+  console.log(MONGO_HOSTNAME)
+  
+  const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
+
+  mongoose.connect(url,{ useNewUrlParser: true,useUnifiedTopology: true }).then( function() {
+    console.log('MongoDB is connected');
+  })
+    .catch( function(err) {
+    console.log(err);
+  });
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
-
-
-// const mongoose = require('mongoose');
-
-// const {
-//   MONGO_USERNAME,
-//   MONGO_PASSWORD,
-//   MONGO_HOSTNAME,
-//   MONGO_PORT,
-//   MONGO_DB
-// } = process.env;
-
-// const options = {
-//   useNewUrlParser: true,
-//   reconnectTries: Number.MAX_VALUE,
-//   reconnectInterval: 500,
-//   connectTimeoutMS: 10000,
-// };
-
-// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-
-// mongoose.connect(url, options).then( function() {
-//   console.log('MongoDB is connected');
-// })
-//   .catch( function(err) {
-//   console.log(err);
-// });
